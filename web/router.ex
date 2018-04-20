115bdad7dc06af2833f20287ed58b7139e376ad8
Hans <hpistor@stetson.edu>
2018-04-20 00:35:29 -0400

    authentication finished i think

--- web/router.ex
+++ web/router.ex
@@ -2,10 +2,17 @@ defmodule Slack.Router do
   use Slack.Web, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
+    plug(Guardian.Plug.VerifyHeader, realm: "Bearer")
+    plug(Guardian.Plug.LoadResource)
   end
 
   scope "/api", Slack do
-    pipe_through :api
+    pipe_through(:api)
+
+    post("/sessions", SessionController, :create)
+    delete("/sessions", SessionController, :delete)
+    post("/sessions/refresh", SessionController, :refresh)
+    resources("/users", UserController, only: [:create])
   end
 end
